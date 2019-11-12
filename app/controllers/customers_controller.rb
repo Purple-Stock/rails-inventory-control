b361400d97896949188e7cc1acaad16c27473401
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-11 22:48:14 -0300

    some changes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
   # GET /customers
   # GET /customers.json
   def index
-    @customers = Customer.all
+    @customers = Customer.last 500
   end
 
   # GET /customers/1
