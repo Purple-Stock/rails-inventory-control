d024b056624a9ea11b36b21f3f1fc3cb7ddcc3f7
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-11 22:19:53 -0300

    Show something active products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where(active: true)
   end
 
   # GET /products/1
