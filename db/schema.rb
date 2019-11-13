df2c47fe74fa641fef27fd5a2265d1b416f40713
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-12 23:07:53 -0300

    Add payment_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_180750) do
+ActiveRecord::Schema.define(version: 2019_11_13_013810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2019_11_02_180750) do
     t.bigint "customer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "payment_type", default: 0
     t.index ["customer_id"], name: "index_sales_on_customer_id"
   end
 
