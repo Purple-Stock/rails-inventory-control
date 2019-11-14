f40b8ecf62539e1ac66d8948101afd6bf8672c08
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-14 13:03:51 -0300

    Add rules to datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,51 @@
 //= require serviceworker-companion
 
 $(document).ready( function () {
-    $('#stockTable').DataTable();
     $('#list').select2();
+
+    $('#stockTable').dataTable( {
+		"language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
+        },
+       	columnDefs: [
+	       { type: 'formatted-num', targets: 0 }
+	    ],
+		"order": [[ 0, "desc" ]],
+		responsive: true,
+     	stateSave: true
+    } );
+
+
+	jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+	    "formatted-num-pre": function ( a ) {
+	        a = (a === "-" || a === "") ? 0 : a.replace( /[^\d\-\.]/g, "" );
+	        return parseFloat( a );
+	    },
+	 
+	    "formatted-num-asc": function ( a, b ) {
+	        return a - b;
+	    },
+	 
+	    "formatted-num-desc": function ( a, b ) {
+	        return b - a;
+	    }
+	} );
+
+	jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+	    "date-uk-pre": function ( a ) {
+	        if (a == null || a == "") {
+	            return 0;
+	        }
+	        var ukDatea = a.split('/');
+	        return (ukDatea[2] + ukDatea[1] + ukDatea[0]) * 1;
+	    },
+	 
+	    "date-uk-asc": function ( a, b ) {
+	        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+	    },
+	 
+	    "date-uk-desc": function ( a, b ) {
+	        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+	    }
+	} );
 } );
