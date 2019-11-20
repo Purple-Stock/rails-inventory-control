9a682ef6fee836f731326ff7e3eacff330801c16
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-20 12:38:36 -0300

    Fix select products

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -1,4 +1,7 @@
 $(document).ready( function () {
     $('#sale_customer_id').select2();
     $('.form-control-sm').select2();
+    $( ".add_fields" ).click(function() {
+        setTimeout(function(){$('.form-control-sm').select2(); }, 1);
+    });
 } );
