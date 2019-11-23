ff542ea4d2bea015e955d3a23b245b6316e259a2
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-23 13:56:07 -0300

    Add skip token

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
