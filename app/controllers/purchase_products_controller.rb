434ef9acca97530c37a891c98a0348673b6b0506
puppe1990 <matheus.puppe90@hotmail.com>
2019-11-25 16:10:43 -0300

    Add created at in purchase product

--- app/controllers/purchase_products_controller.rb
+++ app/controllers/purchase_products_controller.rb
@@ -69,6 +69,6 @@ class PurchaseProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def purchase_product_params
-      params.require(:purchase_product).permit(:quantity, :value, :product_id, :purchaseId)
+      params.require(:purchase_product).permit(:quantity, :value, :product_id, :purchaseId, :created_at)
     end
 end
