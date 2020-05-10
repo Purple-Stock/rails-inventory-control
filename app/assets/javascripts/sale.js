ca6b3ac91dbd8be362e4e1ef1605b7beac0f6256
puppe1990 <matheus.puppe90@hotmail.com>
2020-05-10 20:36:43 -0300

    Add new line on scan qr code

--- app/assets/javascripts/sale.js
+++ app/assets/javascripts/sale.js
@@ -6,27 +6,36 @@ $(document).ready( function () {
     });
 } );
 
+// document.querySelector("#sale_products > div.nested-fields > a").click();
+
 onScan.attachTo(document, {
     suffixKeyCodes: [13], // enter-key expected at the end of a scan
     reactToPaste: true, // Compatibility to built-in scanners in paste-mode (as opposed to keyboard-mode)
     onScan: function(sCode) { // Alternative to document.addEventListener('scan')
-    	console.log(sCode);
         fetchProducts(sCode);
     }
 });
 
+var product_data;
+var inserted;
+
 function fetchProducts(custom_qr) {
     const custom_id = custom_qr.split('#');
-    console.log('/api/v1/products/' + custom_id[3]);
     fetch('/api/v1/products/' + custom_id[3]).then(function (res) {
         if (res.ok) {
             return res.json();
         }
     }).then(function (data) {
-        console.log(data);
-        $('#sale_products').append('<%= j render 'sale_product_fields', animation: 'tetas' %>')
-
+        console.log('hey');
+        product_data = data
+        document.querySelector("#sale_products > .links > a").click();
     }).catch(function (err) {
         return console.log(err);
     });
 }
+
+$('#sale_products').on('cocoon:after-insert', function(e, insertedItem, originalEvent) {
+    insertedItem[0].querySelector('.form-control-sm').value = product_data.id;
+    insertedItem[0].querySelectorAll('input')[0].value = 1;
+    insertedItem[0].querySelectorAll('input')[1].value = product_data.price;
+});
