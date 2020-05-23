e7023dcbb0871cb2d22148a043968f51e5666d20
puppe1990 <matheus.puppe90@hotmail.com>
2020-05-23 16:49:45 -0300

    Config rack cors

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module RailsInventoryControl
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: %i(get post put patch delete options head)
+      end
+    end
   end
 end
