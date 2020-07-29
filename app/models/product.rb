399e70b49d164a53ab75daf836d4b7262d56f567
gabriel fontoura dos santos <gabrielfontoura@me.com>
2020-07-29 12:32:31 -0300

    feat: datatables server side

--- app/models/product.rb
+++ app/models/product.rb
@@ -52,4 +52,23 @@ class Product < ApplicationRecord
     RQRCode::QRCode.new(obj.to_json)
   end
 
+  DATATABLE_COLUMNS = %w[custom_id name].freeze
+
+  class << self
+    def datatable_filter(search_value, search_columns)
+      return all if search_value.blank?
+
+      result = none
+      search_columns.each do |key, value|
+        filter = where("#{DATATABLE_COLUMNS[key.to_i]} ILIKE ?", "%#{search_value}%")
+        result = result.or(filter) if value['searchable']
+      end
+      result
+    end
+
+    def datatable_order(order_column_index, order_dir)
+      order_column_index = 1 if order_column_index == 4
+      order("#{Product::DATATABLE_COLUMNS[order_column_index]} #{order_dir}")
+    end
+  end
 end
