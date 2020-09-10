a150d7523335c8013dfd43b40ad5d6c219abffdb
puppe1990 <matheus.puppe90@hotmail.com>
2020-09-10 11:32:03 -0300

    Improve order integration

--- app/models/simplo_order.rb
+++ app/models/simplo_order.rb
@@ -11,17 +11,22 @@ class SimploOrder < ApplicationRecord
                                             Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
       @order_page['result'].each do |order_page|
         begin
-          sc = SimploOrder.create(client_name: order_page['Wspedido']['cliente_razaosocial'], 
-                                   order_id: order_page['Wspedido']['numero'],
-                                   order_status: order_page['Wspedido']['pedidostatus_id'],
-                                   order_date: Time.parse(order_page['Wspedido']['data_pedido']))
-          order_page['Item'].each do |item|
-            product = Product.where(sku: item['sku']).or(Product.where(extra_sku: item['sku'])).first
-            if product.present?
-              SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: sc.id, product_id: product.id)
-            else
-              SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: sc.id)
+          order = SimploOrder.find_by(order_id: order_page['Wspedido']['numero'])
+          if order.nil?
+            simplo_order = SimploOrder.create(client_name: order_page['Wspedido']['cliente_razaosocial'], 
+                                              order_id: order_page['Wspedido']['numero'],
+                                              order_status: order_page['Wspedido']['pedidostatus_id'],
+                                              order_date: Time.parse(order_page['Wspedido']['data_pedido']))
+            order_page['Item'].each do |item|
+              product = Product.where(sku: item['sku']).or(Product.where(extra_sku: item['sku'])).first
+              if product.present?
+                SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: sc.id, product_id: product.id)
+              else
+                SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: sc.id)
+              end
             end
+          else
+            order.update(order_status: order_page['Wspedido']['pedidostatus_id']) if order.order_status != order_page['Wspedido']['pedidostatus_id']
           end
         rescue ArgumentError
           puts 'erro'
