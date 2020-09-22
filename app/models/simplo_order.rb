4d4e5e02b8024f7f6743bf55aa7f4c7617c47827
puppe1990 <matheus.puppe90@hotmail.com>
2020-09-22 16:02:05 -0300

    Method to update order status from simplo 7

--- app/models/simplo_order.rb
+++ app/models/simplo_order.rb
@@ -2,36 +2,49 @@ class SimploOrder < ApplicationRecord
   has_many :simplo_items
 
   def self.integrate_order_items
-    @order_page = HTTParty.get("https://purchasestore.com.br/ws/wspedidos.json",
-                                headers: { content: 'application/json',
-                                Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+    @order_page = HTTParty.get('https://purchasestore.com.br/ws/wspedidos.json',
+                               headers: { content: 'application/json',
+                                          Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
     (1..@order_page['pagination']['page_count']).each do |i|
       @order_page = HTTParty.get("https://purchasestore.com.br/ws/wspedidos.json?page=#{i}",
                                  headers: { content: 'application/json',
                                             Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
       @order_page['result'].each do |order_page|
-        begin
-          order = SimploOrder.find_by(order_id: order_page['Wspedido']['numero'])
-          if order.nil?
-            simplo_order = SimploOrder.create(client_name: order_page['Wspedido']['cliente_razaosocial'], 
-                                              order_id: order_page['Wspedido']['numero'],
-                                              order_status: order_page['Wspedido']['pedidostatus_id'],
-                                              order_date: Time.parse(order_page['Wspedido']['data_pedido']))
-            order_page['Item'].each do |item|
-              product = Product.where(sku: item['sku']).or(Product.where(extra_sku: item['sku'])).first
-              if product.present?
-                SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: simplo_order.id, product_id: product.id)
-              else
-                SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: simplo_order.id)
-              end
+        order = SimploOrder.find_by(order_id: order_page['Wspedido']['numero'])
+        if order.nil?
+          simplo_order = SimploOrder.create(client_name: order_page['Wspedido']['cliente_razaosocial'],
+                                            order_id: order_page['Wspedido']['numero'],
+                                            order_status: order_page['Wspedido']['pedidostatus_id'],
+                                            order_date: Time.parse(order_page['Wspedido']['data_pedido']))
+          order_page['Item'].each do |item|
+            product = Product.where(sku: item['sku']).or(Product.where(extra_sku: item['sku'])).first
+            if product.present?
+              SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: simplo_order.id, product_id: product.id)
+            else
+              SimploItem.create(sku: item['sku'], quantity: item['quantidade'].to_i, simplo_order_id: simplo_order.id)
             end
-          else
-            order.update(order_status: order_page['Wspedido']['pedidostatus_id']) if order.order_status != order_page['Wspedido']['pedidostatus_id']
           end
-        rescue ArgumentError
-          puts 'erro'
+        else
+          if order.order_status != order_page['Wspedido']['pedidostatus_id']
+            order.update(order_status: order_page['Wspedido']['pedidostatus_id'])
+          end
         end
+      rescue ArgumentError
+        puts 'erro'
       end
+    end
+  end
+
+  def self.update_order_status(order_number)
+    id = (order_number.to_i + 1).to_s
+    data = { Wspedido: { status: { id: id } } }
+    begin
+      HTTParty.put("https://purchasestore.com.br/ws/wspedidos/#{i}.json",
+                   body: data.to_json,
+                   headers: { content: 'application/json',
+                              Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+    rescue ArgumentError
+      puts 'erro'
     end
   end
 end
